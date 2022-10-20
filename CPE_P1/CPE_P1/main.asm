8d07c4a086d6aaa3bf6924e814ac4347975ba1c5
Duncan Truitt <61508835+copper-head@users.noreply.github.com>
2022-10-20 15:36:47 -0500

    Updated code

--- CPE_P1/CPE_P1/main.asm
+++ CPE_P1/CPE_P1/main.asm
@@ -7,63 +7,74 @@
 ;
 ; Notes:
 ;	GPRs reserved for program execution -- R16, R17
-;	GPRs reserved for passing parameters to subroutines -- R18, R19
+;	GPRs reserved for passing parameters to subroutines or junk stuff thats only needed for a couple lines -- R18, R19
 ;	GPRs reserved for use in subroutines -- R20, R21, R22, R23
 
-; Replace with your application code
 start:
-	.DEF CURRENT_NUM = R16; This register needs to be reserved for holding the current level for the led timing (0-24)
-	.DEF NUM_COUNTER = R17; This register is the iterator that the code will use
+	.DEF C_LEVEL = R16; This register needs to be reserved for holding the current level for the led timing (0-24)
+	.DEF ITERATOR = R17; This register is the iterator that the code will use
+
+	.EQU WAIT_MULTIPLIER = 10; ~number of ms for each poll/loop of the program
 	 
     .ORG 0
-	LDI CURRENT_NUM, 0; Current num starts at 0 and gets changed by the user pressing the push button
-	LDI NUM_COUNTER, 24; NUM_COUNTER starts at 24 and decrements 
+	LDI C_LEVEL, 25; Current num starts at 0 and gets changed by the user pressing the push button 
 
 	SBI DDRC, 7; Set LED as output
 	SBI DDRC, 6; Set SPEAKER as output
-	CBI DDRA, 6; Set RIGHT BUTTON as input
+	CBI DDRF, 6; Set RIGHT BUTTON as input
 	CBI DDRD, 4; Set LEFT BUTTON as input
-
 
 	;  --- THE MAIN LOOP BEGINS HERE --- ;
 
-BEGIN:		LDI R24, 0x10
-	LOOP:	DEC R24
-			CALL DELAY_MS
-			BRNE LOOP
-			CALL TOGGLE_LED
-			RJMP BEGIN; Program needs to loop
+BEGIN:		LDI ITERATOR, 192
+	LOOP:	MOV R18, ITERATOR
+			LSR R18
+			LSR R18
+			LSR R18
+			CPI R18, 0
+			BREQ BEGIN ; If Iterator / 8 is 0, Start from the top
+
+			LDI R24, WAIT_MULTIPLIER
+			DEC ITERATOR
+			CP R18, C_LEVEL
+			BRLO T_OFF
+			CALL LED_ON
+			RJMP WAIT	
+	T_OFF:	CALL LED_OFF
+	WAIT:	CALL DELAY_MS
+			DEC R24
+			BRNE WAIT 
+			RJMP LOOP;
+
 
 
 	; THIS SUBROUTINE ADDS A DELAY OF ~1ms ;
 DELAY_MS:
-				LDI R20, 0xFF
-	LOOP1:		LDI	R21, 0xFF
-	LOOP2:		DEC R21
+				LDI R20, 0x26	;Number of iterations for the first loop
+	LOOP1_MS:	LDI	R21, 0x43	;Number of iterations for the second loop
+	LOOP2_MS:	DEC R21			;Decrement value for second loop
+				BRNE LOOP2_MS		;Repeat loop if the value for second loop is not equal to 0
 				NOP
 				NOP
 				NOP
-				BRNE LOOP2
-				DEC R20
 				NOP
 				NOP
 				NOP
-				BRNE LOOP1
-				RET
+				DEC R20			;Decrement value for first loop
+				BRNE LOOP1_MS		;Repeat loop if the value for first loop is not equal to 0
+				RET		
 
 
 
 	; TOGGLE LED SUBROUTINE ;
 TOGGLE_LED:
 				SBIS PORTC, 7
-				RJMP TOGGLE_ON
+				RJMP LED_ON
 				SBIC PORTC, 7
-				RJMP TOGGLE_OFF
+				RJMP LED_OFF
 				RET
 
-	TOGGLE_ON:		SBI PORTC, 7
-					RET
-	TOGGLE_OFF:		CBI PORTC, 7
-					RET
-
-
+LED_ON:			SBI PORTC, 7
+				RET
+LED_OFF:		CBI PORTC, 7
+				RET
